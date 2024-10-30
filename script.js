16d311ac73b9c81f548c0b38c8388648050c006f
Anu <anu.cyto@gmail.com>
2024-10-30 16:55:02 -0400

    amended makeDivs function to create divs of appopriate width and added css flex styling

--- script.js
+++ script.js
@@ -1,17 +1,24 @@
 const container = document.querySelector(".container");
 
 
+//creates and appends square number of divs:
 function makeDivs(number) {
 
-    if (number <= 10) {
+    if (number <= 30) {
+
+        let containerWidth = 600;
 
         for (let i=1; i<=(number*number); i++) {
             const newDiv = document.createElement("div");
+            newDiv.classList.toggle("newDiv");
+            newDiv.style.width = containerWidth/number + "px";
             container.appendChild(newDiv);
         };
+    } else {
+        console.log("too many divs!");
     }
-
-    
 };
 
-makeDivs(2);
+
+
+makeDivs(10);
